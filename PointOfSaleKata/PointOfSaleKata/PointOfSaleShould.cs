b1e2a07cf2ec02d2976261007cf4c6cdf6e11785
evaderrama <evalderrama@domingoalonsogroup.com>
2022-04-06 23:24:57 +0100

    Red test Barcode_23456_should_display_price_of_12_coma_50_euros_()

--- PointOfSaleKata/PointOfSaleKata/PointOfSaleShould.cs
+++ PointOfSaleKata/PointOfSaleKata/PointOfSaleShould.cs
@@ -15,6 +15,17 @@ namespace PointOfSaleKata
 
             Assert.Equal(expectedPrice, result);
         }
+
+        [Fact]
+        public void Barcode_23456_should_display_price_of_12_coma_50_euros_()
+        {
+            string barcode = "23456";
+            double expectedPrice = 12.50;
+
+            double result = PointOfSale.GetPriceForBarcode(barcode);
+
+            Assert.Equal(expectedPrice, result);
+        }
     }
 
     public class PointOfSale
